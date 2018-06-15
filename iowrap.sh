2994e7d97bb7a79cb4d8541961867cf14fee4435
Piotr Gacek <pgacek@linux.pl>
2018-06-15 19:28:36 +0200

    update shitty stuff

--- iowrap.sh
+++ iowrap.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 DATE=$(date +%Y%m%d_%H%M) 
-
+export LC_TIME=C
 
 if [ $# -ne 2 ]; then
 	echo -e "Give disk name and time [in minutes]";
 	exit 1;
 fi 
 
-timeout ${2}m "LC_TIME=C iostat -x  1 |stdbuf -o0 grep $1 > /tmp/disk_stats_raw" &
-timeout ${2}m "LC_TIME=C sar -r 1 > /tmp/mem_stats_raw" &
-timeout ${2}m "LC_TIME=C sar -p 1 > /tmp/cpu_stats_raw"  &
+timeout ${2}m iostat -x  1 |stdbuf -o0 grep $1 > /tmp/disk_stats_raw &
+timeout ${2}m sar -r 1 > /tmp/mem_stats_raw &
+timeout ${2}m sar -p 1 > /tmp/cpu_stats_raw  &
 
 wait
 
@@ -20,9 +20,9 @@ echo "time ; memtotal ; memfree ; membuff ; memcache" > /tmp/mem_stats_${DATE}.c
 
 ##########
 
-cat /tmp/disk_stats | awk '{ time=$1 iops=$4+$5 ; read=$6/1024 ; write=$7/1024 ; await=$10 ; quesi=$9 } { print time ";" iops ";" read ";" write ";"  await ";" quesi }' >>  /tmp/disk_stats_${DATE}.csv 
-cat /tmp/mem_stats | awk '{ time=$1 memtotal=$2+$3 memfree=$3/1024 ; membuff=$6/1024 ; memcache=$7/1024 } { print time ";" memtotal ";" memfree ";" membuff ";" memcache }' >> /tmp/mem_stats_${DATE}.csv 
-cat /tmp/cpu_stats | awk '{ time=$1 usert=$3 ; systemt=$5 ; iowait=$6 } { print time ";" usert ";" systemt ";" iowait }' >> /tmp/cpu_stats_${DATE}.csv 
+cat /tmp/disk_stats_raw | awk '{ time=$1 ; iops=$4+$5 ; read=$6/1024 ; write=$7/1024 ; await=$10 ; quesi=$9 } { print time ";" iops ";" read ";" write ";"  await ";" quesi }' >>  /tmp/disk_stats_${DATE}.csv 
+cat /tmp/mem_stats_raw | awk '{ time=$1 ; memtotal=$2+$3 memfree=$3/1024 ; membuff=$6/1024 ; memcache=$7/1024 } { print time ";" memtotal ";" memfree ";" membuff ";" memcache }' >> /tmp/mem_stats_${DATE}.csv 
+cat /tmp/cpu_stats_raw | awk '{ time=$1 ; usert=$3 ; systemt=$5 ; iowait=$6 } { print time ";" usert ";" systemt ";" iowait }' >> /tmp/cpu_stats_${DATE}.csv 
 
 
 for i in /tmp/disk_stats_${DATE}.csv /tmp/mem_stats_${DATE}.csv  /tmp/cpu_stats_${DATE}.csv ; do
