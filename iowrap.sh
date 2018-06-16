8f39465358c0bbdb60177bd412674af3ae5dac2c
Piotr Gacek <pgacek@linux.pl>
2018-06-16 13:36:45 +0200

    fixed issues

--- iowrap.sh
+++ iowrap.sh
@@ -1,32 +1,42 @@
-#!/bin/bash
+!/bin/bash
 
-DATE=$(date +%Y%m%d_%H%M) 
+DATE=$(date +%Y%m%d_%H%M)
+#sar 24h format.
 export LC_TIME=C
 
+#Provide disk name and how long the monitor is going to work - in minutes
 if [ $# -ne 2 ]; then
-	echo -e "Give disk name and time [in minutes]";
-	exit 1;
-fi 
+        echo -e "Type disk name[like sda] and monitoring time[in minutes]";
+        exit 1;
+fi
 
-timeout ${2}m iostat -x  1 |stdbuf -o0 grep $1 > /tmp/disk_stats_raw &
-timeout ${2}m sar -r 1 > /tmp/mem_stats_raw &
-timeout ${2}m sar -p 1 > /tmp/cpu_stats_raw  &
+#Run tasks parallely in bg
+timeout ${2}m iostat -x  1 |stdbuf -o0 grep $1 > $HOME/disk_stats_raw &
+timeout ${2}m sar -r 1 > $HOME/mem_stats_raw &
+timeout ${2}m sar -p 1 > $HOME/cpu_stats_raw  &
 
 wait
 
 #Format disk,mem,cpu stats to csv
-echo "time ; iops ; read[MB] ; write[MB] ; await[s] ; quesize" > /tmp/disk_stats_${DATE}.csv
-echo "time ; memtotal ; memfree ; membuff ; memcache" > /tmp/mem_stats_${DATE}.csv 
+echo "iops ; read[MB] ; write[MB] ; await ; quesize" > $HOME/disk_stats_nottime
+echo "time ; memtotal ; memfree ; membuff/cache" > $HOME/mem_stats_${DATE}.csv
+echo "time ; %user ; %system ; %iowait" > $HOME/cpu_stats_${DATE}.csv
 
-##########
+#sar in 1st column shows time, iostat doesn't
+tail -n +4 $HOME/disk_stats_raw | awk '{ iops=$4+$5 ; read=$6/1024 ; write=$7/1024 ; await=$10 ; quesi=$9 } { print iops ";" read ";" write ";"  await ";" quesi }' >>  $HOME/disk_stats_nottime
+tail -n +4 $HOME/mem_stats_raw | awk '{ time=$1 ; memtotal=($2+$3)/1024 ; memfree=$2/1024 ; membuffcache=($5+$6)/1024 } { print time ";" memtotal ";" memfree ";" membuffcache }' >> $HOME/mem_stats_${DATE}.csv
+tail -n +4 $HOME/cpu_stats_raw | awk '{ time=$1 ; usert=$3 ; systemt=$5 ; iowait=$6 } { print time ";" usert ";" systemt ";" iowait }' >> $HOME/cpu_stats_${DATE}.csv
 
-cat /tmp/disk_stats_raw | awk '{ time=$1 ; iops=$4+$5 ; read=$6/1024 ; write=$7/1024 ; await=$10 ; quesi=$9 } { print time ";" iops ";" read ";" write ";"  await ";" quesi }' >>  /tmp/disk_stats_${DATE}.csv 
-cat /tmp/mem_stats_raw | awk '{ time=$1 ; memtotal=$2+$3 ; memfree=$3/1024 ; membuff=$6/1024 ; memcache=$7/1024 } { print time ";" memtotal ";" memfree ";" membuff ";" memcache }' >> /tmp/mem_stats_${DATE}.csv 
-cat /tmp/cpu_stats_raw | awk '{ time=$1 ; usert=$3 ; systemt=$5 ; iowait=$6 } { print time ";" usert ";" systemt ";" iowait }' >> /tmp/cpu_stats_${DATE}.csv 
+#get time from another file
+cat $HOME/mem_stats_${DATE}.csv | awk -F ';' '{ print $1 }' > $HOME/time
 
+#fetch time and disk stats
+paste -d ';' $HOME/time $HOME/disk_stats_nottime > $HOME/disk_stats_${DATE}.csv
 
-for i in /tmp/disk_stats_${DATE}.csv /tmp/mem_stats_${DATE}.csv  /tmp/cpu_stats_${DATE}.csv ; do
-	sed -i 's/\./\,/g' $i;
+#delete unused files
+rm -f $HOME/time $HOME/disk_stats_nottime
+
+#change '.' to ','
+for i in $HOME/disk_stats_${DATE}.csv $HOME/mem_stats_${DATE}.csv  $HOME/cpu_stats_${DATE}.csv ; do
+        sed -i 's/\./\,/g' $i;
 done
-
-
